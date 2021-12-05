63cb9dc00abf3523a608958d7f98a9558daf7005
Thomas Chartron <thomas.chartron@gmail.com>
2021-12-05 20:18:58 +0100

    update

--- deploy-github.sh
+++ deploy-github.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 # If a command fails then the deploy stops
-set -e
+# set -e
 
 if [ -d "public" ]
 then
     echo -e "\033[0;32mRemoving public folder except .git and README.md and CNAME \033[0m\n"
     cd public
-    find . -not -name '.git' -not -name 'README.md' -not -name 'CNAME' -delete
+    # find . -not -name '.git' -not -name 'README.md' -not -name 'CNAME' -delete
+    find . -not -name '.git' -not -name 'README.md' -not -name 'CNAME' -not -name '.DS_Store' -not -name '.' -not -name '..' -exec rm -rf -- {} +
     cd ..
 fi
 
